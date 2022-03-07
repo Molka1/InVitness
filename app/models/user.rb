e435fbda5e0a75879c4a3e9fd4e65f0f33380f5f
Andrea Terminel <andrea.terminel@gmail.com>
2022-03-07 14:39:09 +0000

    branded map monday progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :challenges, dependent: :destroy
   has_many :exercises,  dependent: :destroy
   has_many :user_challenges, dependent: :destroy
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
